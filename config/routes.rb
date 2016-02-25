4a003635cb11b685c6460ddaaab8e2dbffa5203d
dpaniagua0 <dpaniagua0@gmail.com>
2016-02-25 14:36:42 -0600

    Fix create, add flash messages, create show

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'home', to: 'pages#home'
 
-  resource :articles
+  resources :articles 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
